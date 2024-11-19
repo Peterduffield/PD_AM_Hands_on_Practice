ac3fa0c03dfe6fb8ab14e739ead55c151e0b1faa
Aldo Mora <aldo_mora@hakkoda.io>
2024-11-19 18:41:51 +0000

    Update 2

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,12 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from PD_AM_HANDS_ON_PRACTICE.jaffle_shop.customers
+    select * from {{ ref('stg_customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from PD_AM_HANDS_ON_PRACTICE.jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 
 ),
 
